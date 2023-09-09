c232eae9ac1e9a75bdef02133d5ae5ab65806be9
Mohsin <mohsin@Mohsins-MacBook-Pro.local>
2023-09-09 22:30:02 +0600

    fixed the eslinting

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,65 +1,63 @@
 // auth/auth.controller.ts
-import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserLoginDto } from './user-login.dto';
 import { RefreshTokenDto } from './refresh-token.dto';
 import { JwtAuthGuard } from './jwt-auth.guard';
 import { UserService } from 'src/user/user.service';
-import { JwtService } from '@nestjs/jwt';
 import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly userService: UserService,
-        private readonly jwtService: JwtService,
-        private readonly authService: AuthService) { }
+  constructor(
+    private readonly userService: UserService,
+    private readonly authService: AuthService,
+  ) {}
 
-    @UseGuards(LocalAuthGuard)
-    @Post('login')
-    async login(@Body() userLoginDto: UserLoginDto) {
-        // Call your authentication service to validate the user's credentials
-        const user = await this.authService.validateUser(
-            userLoginDto.username,
-            userLoginDto.password,
-        );
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Body() userLoginDto: UserLoginDto) {
+    // Call your authentication service to validate the user's credentials
+    const user = await this.authService.validateUser(
+      userLoginDto.username,
+      userLoginDto.password,
+    );
 
-        if (!user) {
-            // If authentication fails, return an appropriate response
-            return { message: 'Invalid credentials' };
-        }
-
-        // If authentication is successful, generate a JWT token
-        const accessToken = this.authService.generateAccessToken(user);
-
-        // Return the JWT token as a response
-        return { accessToken };
+    if (!user) {
+      // If authentication fails, return an appropriate response
+      return { message: 'Invalid credentials' };
     }
 
-    @UseGuards(JwtAuthGuard)
-    @Post('refresh-token')
-    async refreshAccessToken(refreshTokenDto: RefreshTokenDto): Promise<string> {
-        const { refreshToken } = refreshTokenDto;
+    // If authentication is successful, generate a JWT token
+    const accessToken = this.authService.generateAccessToken(user);
 
-        // Validate the refresh token here (e.g., check against a database)
-        // Replace the following code with your own logic
-        if (refreshToken !== 'valid_refresh_token') {
-            throw new Error('Invalid refresh token');
-        }
+    // Return the JWT token as a response
+    return { accessToken };
+  }
 
-        // Find the user associated with the refresh token (you might need to modify your User service)
-        const user = await this.userService.findByRefreshToken(refreshToken);
+  @UseGuards(JwtAuthGuard)
+  @Post('refresh-token')
+  async refreshAccessToken(refreshTokenDto: RefreshTokenDto): Promise<string> {
+    const { refreshToken } = refreshTokenDto;
 
-        if (!user) {
-            throw new Error('User not found');
-        }
-
-        // Generate a new access token
-        const accessToken = this.authService.generateAccessToken(user);
-
-        return accessToken;
+    // Validate the refresh token here (e.g., check against a database)
+    // Replace the following code with your own logic
+    if (refreshToken !== 'valid_refresh_token') {
+      throw new Error('Invalid refresh token');
     }
-    // async generateAccessToken(user: User): Promise<string> {
-    //     const payload = { username: user.username, sub: user.id };
-    //     return this.jwtService.sign(payload);
-    // }
+
+    // Find the user associated with the refresh token (you might need to modify your User service)
+    const user = await this.userService.findByRefreshToken(refreshToken);
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    // Generate a new access token
+    return this.authService.generateAccessToken(user);
+  }
+  // async generateAccessToken(user: User): Promise<string> {
+  //     const payload = { username: user.username, sub: user.id };
+  //     return this.jwtService.sign(payload);
+  // }
 }
